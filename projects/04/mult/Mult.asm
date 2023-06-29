04babd9d8c5ecc96be359edc87ccfe406c45cb24
Arhan Chaudhary <arhan.ch@gmail.com>
2023-06-29 01:31:10 -0700

    Mult assembly code

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,30 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+// def mult(R0, R1) {
+//     R2 = 0;
+//     while (R0 > 0) {
+//         R0 -= 1;
+//         R2 += R1;
+//     }
+//     return R2;
+// }
+
+    @R2
+    M=0
+(LOOP)
+    @R0
+    D=M
+    @END
+    D;JLE
+    @R0
+    M=M-1
+    @R1
+    D=M
+    @R2
+    M=M+D
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
