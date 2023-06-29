d5deed5c8e25a7a642daa3cbabf0cf888aa98450
Arhan Chaudhary <arhan.ch@gmail.com>
2023-06-29 03:43:49 -0700

    Fill assembly code

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -10,5 +10,42 @@
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
-
-// Put your code here.
+(LISTENER_LOOP)
+    @SCREEN
+    D=A
+    @curr
+    M=D
+    @KBD
+    D=M
+    @MAKE_BLACK
+    D;JGT
+    @MAKE_WHITE
+    0;JMP
+(MAKE_BLACK)
+    @curr
+    D=M
+    @SCREEN
+    A=D
+    M=-1
+    @curr
+    MD=M+1
+    @KBD
+    D=D-A
+    @LISTENER_LOOP
+    D;JEQ
+    @MAKE_BLACK
+    0;JMP
+(MAKE_WHITE)
+    @curr
+    D=M
+    @SCREEN
+    A=D
+    M=0
+    @curr
+    MD=M+1
+    @KBD
+    D=D-A
+    @LISTENER_LOOP
+    D;JEQ
+    @MAKE_WHITE
+    0;JMP
