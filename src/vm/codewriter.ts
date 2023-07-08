047c64d6e006908c187206f5d067d9ca4cc0764a
Arhan Chaudhary <arhan.ch@gmail.com>
2023-07-08 09:12:30 -0400

    Rest of arithmetic commands

--- src/vm/codewriter.ts
+++ src/vm/codewriter.ts
@@ -3,6 +3,7 @@ import NANDException from '../core/exceptions';
 
 export default class CodeWriter {
     private fileStream: WriteStream;
+    static labelCount: number = 0;
 
     constructor(file: string) {
         this.fileStream = fs.createWriteStream(file.replace(".vm", ".asm"));
@@ -10,21 +11,90 @@ export default class CodeWriter {
 
     public writeArithmetic(command: string): void {
         
-        let out: Array<string>;
-        switch (command) {
-            case 'add':
-                // sp = sp - 1, ram[sp - 1] = ram[sp - 1] + ram[sp], M=M+D
-                out = [
-                    '@SP',
-                    'AM=M-1',
-                    'D=A',
-                    'A=A-1',
-                    'M=M+D',
-                ]
-                break;
-            default:
-                throw new NANDException("Invalid vm command: " + command);
-        }
+        const out: Array<string> | undefined = {
+            'add': [
+                '@SP',
+                'AM=M-1',
+                'D=M',
+                'A=A-1',
+                'M=M+D',
+            ],
+            'sub': [
+                '@SP',
+                'AM=M-1',
+                'D=M',
+                'A=A-1',
+                'M=M-D',
+            ],
+            'neg': [
+                '@SP',
+                'A=M-1',
+                'M=-M',
+            ],
+            'eq': [
+                '@SP',
+                'AM=M-1',
+                'D=M',
+                'A=A-1',
+                'D=M-D',
+                'M=0',
+                '@FALSE_' + CodeWriter.labelCount,
+                'D;JNE',
+                '@SP',
+                'A=M-1',
+                'M=-1',
+                '(FALSE_' + CodeWriter.labelCount++ + ')',
+            ],
+            'gt': [
+                '@SP',
+                'AM=M-1',
+                'D=M',
+                'A=A-1',
+                'D=M-D',
+                'M=0',
+                '@FALSE_' + CodeWriter.labelCount,
+                'D;JLE',
+                '@SP',
+                'A=M-1',
+                'M=-1',
+                '(FALSE_' + CodeWriter.labelCount++ + ')',
+            ],
+            'lt': [
+                '@SP',
+                'AM=M-1',
+                'D=M',
+                'A=A-1',
+                'D=M-D',
+                'M=0',
+                '@FALSE_' + CodeWriter.labelCount,
+                'D;JGE',
+                '@SP',
+                'A=M-1',
+                'M=-1',
+                '(FALSE_' + CodeWriter.labelCount++ + ')',
+            ],
+            'and': [
+                '@SP',
+                'AM=M-1',
+                'D=M',
+                'A=A-1',
+                'M=M&D',
+            ],
+            'or': [
+                '@SP',
+                'AM=M-1',
+                'D=M',
+                'A=A-1',
+                'M=M|D',
+            ],
+            'not': [
+                '@SP',
+                'A=M-1',
+                'M=!M',
+            ],
+        }[command];
+        if (out === undefined)
+            throw new NANDException("Invalid vm command: " + command);
         this.fileStream.write(out.join('\n'));
     }
 
