7a544f4779acd6fef6f5e4fc8b91c603a34e8ef0
Arhan Chaudhary <arhan.ch@gmail.com>
2023-07-11 00:52:13 -0400

    VM compile directories

--- src/vm/main.ts
+++ src/vm/main.ts
@@ -1,40 +1,57 @@
-// TODO: If the program’s argument is a directory name rather than a file name, the main program should process all the .vm files in this directory. In doing so, it should use a separate Parser for handling each input file and a single CodeWriter for handling the output.
 import CodeWriter from "./codewriter";
 import Parser, { CommandType } from "./parser";
+import fs from "fs";
 
-const file = process.argv[process.argv.length - 1];
-
-const parser: Parser = new Parser(file);
-const codeWriter: CodeWriter = new CodeWriter(file);
-while (parser.advance()) {
-    switch (parser.commandType()) {
-        case CommandType.C_ARITHMETIC:
-            codeWriter.writeArithmetic(parser.arg1());
-            break;
-        case CommandType.C_POP:
-            codeWriter.writePop(parser.arg1(), parser.arg2());
-            break;
-        case CommandType.C_PUSH:
-            codeWriter.writePush(parser.arg1(), parser.arg2());
-            break;
-        case CommandType.C_LABEL:
-            codeWriter.writeLabel(parser.arg1());
-            break;
-        case CommandType.C_GOTO:
-            codeWriter.writeGoto(parser.arg1());
-            break;
-        case CommandType.C_IF:
-            codeWriter.writeIf(parser.arg1());
-            break;
-        case CommandType.C_FUNCTION:
-            codeWriter.writeFunction(parser.arg1(), parser.arg2());
-            break;
-        case CommandType.C_RETURN:
-            codeWriter.writeReturn();
-            break;
-        case CommandType.C_CALL:
-            codeWriter.writeCall(parser.arg1(), parser.arg2());
-            break;
-    }
+let path: string = process.argv[process.argv.length - 1];
+if (path[path.length - 1] === '/') {
+    path = path.slice(0, -1);
 }
+
+let files: string[];
+let codeWriter: CodeWriter;
+if (path.endsWith('.vm')) {
+    files = [path];
+    codeWriter = new CodeWriter(path.substring(0, path.length - 3) + '.asm');
+} else {
+    files = fs.readdirSync(path);
+    const tmp = path.split('/');
+    codeWriter = new CodeWriter(`${path}/${tmp[tmp.length - 1]}.asm`);
+}
+codeWriter.writeInit();
+files.forEach(file => {
+    if (!file.endsWith('.vm')) return;
+
+    const parser: Parser = new Parser(path + '/' + file);
+    while (parser.advance()) {
+        switch (parser.commandType()) {
+            case CommandType.C_ARITHMETIC:
+                codeWriter.writeArithmetic(parser.arg1());
+                break;
+            case CommandType.C_POP:
+                codeWriter.writePop(parser.arg1(), parser.arg2());
+                break;
+            case CommandType.C_PUSH:
+                codeWriter.writePush(parser.arg1(), parser.arg2());
+                break;
+            case CommandType.C_LABEL:
+                codeWriter.writeLabel(parser.arg1());
+                break;
+            case CommandType.C_GOTO:
+                codeWriter.writeGoto(parser.arg1());
+                break;
+            case CommandType.C_IF:
+                codeWriter.writeIf(parser.arg1());
+                break;
+            case CommandType.C_FUNCTION:
+                codeWriter.writeFunction(parser.arg1(), parser.arg2());
+                break;
+            case CommandType.C_RETURN:
+                codeWriter.writeReturn();
+                break;
+            case CommandType.C_CALL:
+                codeWriter.writeCall(parser.arg1(), parser.arg2());
+                break;
+        }
+    }
+});
 codeWriter.close();
