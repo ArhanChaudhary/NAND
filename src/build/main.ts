2a52e37ede3bf7c72c5350f680f5c442d79b65b7
Arhan Chaudhary <arhan.ch@gmail.com>
2023-07-12 21:23:58 -0400

    Builder file

---
+++ src/build/main.ts
@@ -0,0 +1,14 @@
+import { execSync } from 'child_process';
+
+function compileAllStages(inputDirectory: string) {
+    execSync(`~/Desktop/nand2tetris/tools/JackCompiler.sh ${inputDirectory}`);
+    execSync(`ts-node vm/main ${inputDirectory}`);
+    execSync(`ts-node assembler/main ${inputDirectory}/${inputDirectory.split('/')[inputDirectory.split('/').length - 1]}.asm`);
+    console.log('All stages compiled successfully.');
+}
+
+let inputDirectory = process.argv[process.argv.length - 1];
+if (inputDirectory[inputDirectory.length - 1] === '/') {
+    inputDirectory = inputDirectory.slice(0, -1);
+}
+compileAllStages(inputDirectory);
