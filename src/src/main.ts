47f7f0d8fe18e7463a7c64cb4da5f3ff6d3d4c3e
Arhan Chaudhary <arhan.ch@gmail.com>
2023-08-20 18:38:52 -0400

    play around with svelte

--- src/src/main.ts
+++ src/src/main.ts
@@ -1,9 +1,7 @@
-import './app.css'
-import './bootstrap.js'
 import App from './App.svelte'
 
 const app = new App({
-  target: document.getElementById('app'),
+  target: document.getElementById('app')!,
 })
 
 export default app
