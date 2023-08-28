1b3ef7c97d9ae7d2c991f4c827c34e0f1e6114dc
Arhan Chaudhary <arhan.ch@gmail.com>
2023-08-28 00:05:34 -0400

    Drop overflown screen frames

--- src/app/screen.ts
+++ src/app/screen.ts
@@ -3,13 +3,23 @@ import * as computer from "core";
 
 async function initialize() {
   let ctx: OffscreenCanvasRenderingContext2D;
+  let running = false;
   self.addEventListener('message', function(e) {
     if (!ctx) {
       ctx = e.data.getContext('2d');
       ctx.fillStyle = 'black';
       return;
     }
+    if (running) return;
+    running = true;
     computer.render(ctx as unknown as CanvasRenderingContext2D, e.data);
+    // we need a settimeout and can't just put running = false alone here
+    // because the computer.render call is synchronous; that is even if there
+    // is another message it will first wait for the render to finish and then
+    // instantly do the render in the other message making the running = false
+    // completely pointless. Add a small buffer to make sure the next message
+    // is processed early enough
+    setTimeout(() => {running = false}, 0);
   });
 
   self.postMessage('ready');
