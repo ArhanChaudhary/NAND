272b67223119eb147952186b9f0202a5721ce839
Arhan Chaudhary <arhan.ch@gmail.com>
2023-10-19 17:30:47 -0400

    get_ram API

--- src/core/builtins.rs
+++ src/core/builtins.rs
@@ -131,10 +131,10 @@ pub fn ram16k(in_: u16, load: bool, address: u16) -> u16 {
     out
 }
 
-// #[wasm_bindgen(js_name=getRAM)]
-// pub fn get_ram() -> Vec<u16> {
-// 	unsafe { RAM16K_MEMORY.clone() }
-// }
+#[wasm_bindgen(js_name=getRAM)]
+pub fn get_ram() -> Vec<u16> {
+	unsafe { RAM16K_MEMORY.clone() }
+}
 
 static mut CURRENT_KEY: u16 = 0;
 #[wasm_bindgen]
