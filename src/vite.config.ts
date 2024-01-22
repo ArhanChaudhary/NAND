427707e42a7df081385cb2da9b4687bb63e10614
Arhan Chaudhary <arhan.ch@gmail.com>
2024-01-22 05:21:35 -0500

    Configure PWA to feel cool

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import wasm from "vite-plugin-wasm";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { VitePWA } from 'vite-plugin-pwa';
 import sveltePreprocess from "svelte-preprocess";
 import topLevelAwait from "vite-plugin-top-level-await";
 
@@ -14,6 +15,23 @@ export default defineConfig({
   },
   plugins: [
     wasm(),
+    VitePWA({
+      manifest: {
+        name: "NAND",
+        short_name: "NAND",
+        display: "fullscreen",
+        theme_color: "black",
+        background_color: "hsl(0, 0%, 83%)",
+        icons: [
+          {
+            src: "/favicon.ico",
+            sizes: "450x450",
+            type: "image/png",
+            purpose: "any",
+          },
+        ],
+      }
+    }),
     svelte({ preprocess: sveltePreprocess({ postcss: true }) }),
     topLevelAwait(),
   ],
