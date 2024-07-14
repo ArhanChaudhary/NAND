c7953739e3ce2c2718f1321840ddb5261675b054
Arhan Chaudhary <arhan.ch@gmail.com>
2024-07-14 00:43:19 -0700

    Reapply "Step through program draft"
    This reverts commit 78b843e8dfffa21debf1113fd872d6cbb4ed5f05.

--- src/app/stores.ts
+++ src/app/stores.ts
@@ -3,6 +3,7 @@ import { CompilerError } from "../compiler/exceptions";
 export const IDEContext = writable<{ fileName: string; file: string[] }[]>(
   JSON.parse(localStorage.getItem("IDEContext") as string) || []
 );
+export const computerIsRunning = writable<boolean | null>(null);
 export const shouldResetAndStart = writable(true);
 export const activeTabName = writable<null | string>(null);
 export const computerMemory = writable({
